865e45f1577f42c5d8ef61eca74cabe50614a042
Alu-0 <N00507797@upn.pe>
2026-09-23 09:40:03 -0500

    Primer Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//Funcion de Imprimir Notas
+
+static void ImprimirNotas(int[] notas)
+{
+    for(int i=0; i < notas.Length; i++)
+    {
+        System.Console.WriteLine(notas[i]);
+    }
+}
+int[]notas={15,14,13,16,17};
+
+//Imprimir Notas
+ImprimirNotas(notas);
