5d64b013bb054e62950bab98f4750f7667a4288f
Alu-0 <N00507797@upn.pe>
2026-09-23 10:16:17 -0500

    Tercer Commit

--- Program.cs
+++ Program.cs
@@ -1,22 +1,33 @@
-﻿//función de Imprimir notas
+﻿//Función de Imprimir notas
 static void ImprimirNotas(int[] notas)
 {
-    for(int i=0; i<notas.Length; i++)
+    for (int i = 0; i < notas.Length; i++)
     {
         Console.WriteLine(notas[i]);
     }
 }
 
-int[] notas={15,18,12,17,20};
+//Función para calcular el promedio
+static double CalcularPromedio(int[] notas)
+{
+    int suma = 0;
+
+    for (int i = 0; i < notas.Length; i++)
+    {
+        suma += notas[i];
+    }
+
+    double promedio = (double)suma / notas.Length;
+
+    return promedio;
+}
+
+int[] notas = { 15, 18, 12, 17, 20 };
 
 //Imprimir las notas
 ImprimirNotas(notas);
 
-//Calcular el promedio de las notas
-int suma=0;
-for(int i=0; i<notas.Length; i++)
-{
-    suma+=notas[i];
-}
-double promedio = (double)suma / notas.Length;
+//Calcular el promedio
+double promedio = CalcularPromedio(notas);
+
 Console.WriteLine("El promedio es: {0}", promedio);
--- obj/Debug/net10.0/Ejemplo_1.AssemblyInfo.cs
+++ obj/Debug/net10.0/Ejemplo_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ejemplo_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+865e45f1577f42c5d8ef61eca74cabe50614a042")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e3321e1d651d2e9ef365d7012e3084ec7c649cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ejemplo_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ejemplo_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
